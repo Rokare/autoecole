fdf26774c5cda151959c204507b9b4a7e2e26bbf
Rokare <32900647+Rokare@users.noreply.github.com>
2018-02-19 13:18:11 +0100

    moniteur2

--- Classe/moniteur.class.php
+++ Classe/moniteur.class.php
@@ -2,48 +2,18 @@
 
 
 class Moniteur extends Tiers{
-    private $id, $date_embauche, $date_fin, $nom, $prenom, $date_naissance, $telephone;
+    protected $date_embauche, $date_fin;
     public function __construct()
     {
-        $this->id=0;
+        parent::__construct();
+ 
         $this->date_embauche = "";
         $this->date_fin = "";
-        $this->date_naissance ="";
-        $this->telephone = "";
-        $this->prenom = "";
-        $this->nom = "";
-
+       
     }
 
 
-    public function getId()
-    {
-        return $this->id;
-    }
-
-    public function getNom(){
-        
-        return $this->nom;
-        
-    }
-    
-    public function getPrenom(){
-        
-        return $this->prenom;
-        
-    }
-
-    public function getDateNaissance(){
-        
-        return $this->date_naissance;
-        
-    }
-    
-    public function getTelephone(){
-        
-        return $this->telephone;
-        
-    }
+ 
     
     public function getDateEmbauche(){
         
@@ -59,30 +29,7 @@ class Moniteur extends Tiers{
     
     
     /* SETTERS */
-    public function setNom($nom){
-        
-        $this->nom = $nom;
-        
-    }
-    
-    public function setPrenom($prenom){
-        
-        $this->prenom = $prenom;
-        
-    }
-
-    public function setDateNaissance($date_naissance){
-        
-        $this->date_naissance = $date_naissance;
-        
-    }
-    
-    public function setTelephone($telephone){
-        
-        $this->telephone = $telephone;
-        
-    }
-    
+  
     public function setDateEmbauche($date_embauche){
         
         $this->date_embauche = $date_embauche;
@@ -94,10 +41,6 @@ class Moniteur extends Tiers{
         
     }
 
-    public function setId($id)
-    {
-        $this->id = $id;
-    }
 
 
 
