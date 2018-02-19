ea7c1750bd2ed409d59c483ed6ea8ce040253c10
Malek <dahmouche.abdelmalek@gmail.com>
2018-02-19 13:38:05 +0100

    Ajout classes Salarie et Etudiant

---
+++ Controleur/classes/salarie.class.php
@@ -0,0 +1,25 @@
+<?php
+
+class Salarie extends Candidat{
+
+    private $nomEntreprise;
+
+    public function __construct{
+
+        parent::__construct();
+        $this->nomEntreprise = "";
+    }
+
+    /*GETTERS*/
+    public function getNomEntreprise(){
+        return $this->nomEntreprise;
+    }
+
+    /*SETTERS*/
+    public function setNomEntreprise(){
+        $this->nomEntreprise;
+    }
+}
+
+
+?>
