62cd2ee11205a2a662e58815287a927b3c95aa9e
Malek <dahmouche.abdelmalek@gmail.com>
2018-02-19 13:12:53 +0100

    Ajout classe Candidat

--- Controleur/classes/candidat.class.php
+++ Controleur/classes/candidat.class.php
@@ -1,4 +1,38 @@
 <?php
 
+class Candidat extends Tiers{
+
+    protected $dateInscription, $modeFacturation;
+
+    public function __construct(){
+        
+        parent::__construct();
+        $this->dateInscription = "";
+        $this->modeFacturation = "";
+
+    }
+
+
+    /* GETTERS */
+    public function getDateInscription(){
+        return $this->dateInscription;
+    }
+
+    public function getModeFacturation(){
+        return $this->modeFacturation;
+    }
+
+
+    /*SETTERS*/
+    public function setDateInscription($dateInscription){
+        $this->dateInscription = $dateInscription;
+    }
+
+    public function setModeFacturation($modeFacturation){
+        $this->modeFacturation = $modeFacturation;
+    }
+
+}
+
 
 ?>
