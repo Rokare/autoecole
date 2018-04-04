ad5d1dea4ed7688b9e07bf905dc31099e8df6751
Rokare <32900647+Rokare@users.noreply.github.com>
2018-04-04 14:15:31 +0200

    update 1.047
    calendar fonctionnel

--- test_calendar/dbconn.php
+++ test_calendar/dbconn.php
@@ -1,3 +1,3 @@
 <?php
-$connection = mysqli_connect('localhost','dbuser','dbpass','dbname') or die(mysqli_error($connection));
+$connection = mysqli_connect('localhost','root','','ppe') or die(mysqli_error($connection));
 ?>
