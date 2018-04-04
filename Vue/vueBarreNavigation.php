0d92d0d8d4eb97ff4d0ae82a00706b4ec67f485e
Rokare <32900647+Rokare@users.noreply.github.com>
2018-04-04 15:40:30 +0200

    update 1.048
    navbar améliorée

--- Vue/vueBarreNavigation.php
+++ Vue/vueBarreNavigation.php
@@ -1,16 +1,45 @@
+
+
 <nav class="navbar navbar-expand-lg bg-dark">
-  <!-- Navbar content -->
+
 
   <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
     
-    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+
     
       
-    </ul>
-    <form class="form-inline my-2 my-lg-0">
     
-      <a class="btn btn-outline-secondary my-2 my-sm-0" href="index.php?page=1">Inscription</a>
-      <a class="btn btn-outline-success my-2 my-sm-0" href="#">Connexion</a>
-    </form>
+    
+    
+      
+      <ul class="nav navbar-nav navbar-right ml-auto">
+      <li class="nav-item">			
+      <a class="nav-link text-center btn btn-outline-secondary my-2 my-sm-0" href="index.php?page=1">Inscription</a>
+      
+      </li>
+			<li class="nav-item">
+				<a data-toggle="dropdown" class="nav-link dropdown-toggle btn btn-outline-success  my-2 my-sm-0" href="#">Connexion</a>
+				<ul class="dropdown-menu login-form">					
+					<li>
+						<form action="/examples/actions/confirmation.php" method="post">
+							<div class="form-group">
+								<label>Identifiant</label>
+								<input type="text" class="form-control" required="required">
+							</div>
+							<div class="form-group">
+								<div class="clearfix">
+									<label>Mot de Passe</label>
+									<a href="#" class="pull-right text-muted"><small>Oublié?</small></a>
+								</div>
+								
+								<input type="password" class="form-control" required="required">
+							</div>
+							<input type="submit" class="btn btn-danger btn-block" value="Connexion">
+						</form>
+					</li>
+				</ul>
+			</li>
+    
   </div>
 </nav>
+
---
+++ Vue/vueConnexion.php
@@ -0,0 +1,22 @@
+
+
+                <a class="nav-link dropdown co coco btn btn-success " href="#" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Connexion</a>
+                <form class="dropdown-menu dr" method="post" action="login.php">
+                  <div class="container  form-group"> 
+                   <div class="text-center de"><h4><b>Connexion</b></h4></div>
+                   <label class="lab">Identifiant</label>
+                    <br><input class="form-control" type="text" placeholder="Identifiant" name="login">
+                    <label class="lab">Mot de passe</label>
+                    <input class="form-control" type="password" placeholder="Mot de passe" name="mdp">
+                    <!--<button type="reset">Annuler</button> <button class="btn btn-primary btn-lg" type="submit">Connexion</button>-->
+                    <input type="checkbox" tabindex="3" name="remember" id="remember">
+                    <label for="remember">Se souvenir</label>
+                    <input type="submit" name="submit" id="login-submit" tabindex="4" class="form-control btn btn-success">
+                    <a href="password.php" tabindex="5" class="forgot-password">Mot de passe oublié?</a>      
+
+
+                     </div>
+                </form>
+
+
+            
