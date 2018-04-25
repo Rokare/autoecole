fa54b479ba7d331c002a06859d1c1d83ab50e0f8
Rokare <32900647+Rokare@users.noreply.github.com>
2018-04-25 14:14:59 +0200

    update 1.062
    repair 2

--- index.php
+++ index.php
@@ -6,11 +6,11 @@
 ?>
 
 <html lang="fr">
-   <head>     
+   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-       
-       
+
+
 
     <link href="Vue/bootstrap-4.0.0-beta.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="Vue/style/style.css">
@@ -20,35 +20,38 @@
 
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>   
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
    </head>
-   
+
     <body>
         <div class="scrollbar" id="style-1">
       <div class="force-overflow"></div>
     </div>
-    
+
         <?php
                 $page = (isset($_GET['page']))?$_GET['page'] : 0;
                 $unControleur = new Controleur("localhost","adlauto","root","","tiers");
                 include "Vue/vueBarreNavigation.php";
-            
+
                 switch($page)
                 {
-                        
+
                     case 0:
                         include "Vue/vue_accueil.php";
                         break;
-                    case 1:           
+                    case 1:
                         include "Vue/vueinscription.php";
                         if(isset($_POST['inscription'])){
-                            //INSERTION D'UN NOUVEAU TIERS
-                            $unTiers = new Tiers();
-                            $unTiers->renseigner($_POST);
-                            
-                            $unControleur->insert($unTiers);
-                            echo "<br> Insertion réussie <br>";
-                        }
+                           //INSERTION D'UN NOUVEAU TIERS
+                           $unTiers = new Tiers();
+                           $unTiers->renseigner($_POST);
+                           $unControleur->insert($unTiers);
+                           $unCandidat = new Candidat();
+                           $unCandidat->renseigner($_POST);
+                           $unControleur->insert($unCandidat);
+                           echo "<br> Insertion réussie <br>";
+
+                       }
                     break;
                     case 2:
                         include "Vue/tarif.php";
@@ -56,7 +59,7 @@
                 }
             ?>
     </body>
-    
+
 </html>
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script>window.jQuery || document.write('<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"><\/script>')</script>
