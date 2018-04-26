ca2685b9ed4c4cec6ed318e0a9dde6cd137bced0
Rokare <32900647+Rokare@users.noreply.github.com>
2018-04-26 22:28:20 +0200

    udpate 1.067

--- Modele/SQL/procedure.sql
+++ Modele/SQL/procedure.sql
@@ -1,47 +1,44 @@
-DROP FUNCTION IF EXISTS statut;
-DELIMITER //
-CREATE FUNCTION statut(log text, mdp text)
-returns varchar(20)
-		DETERMINISTIC
 
-	BEGIN
-	
-	DECLARE statut varchar(20);
-	DECLARE cand int;
-	DECLARE perso int;
-	DECLARE moni int;
-	
-	select count(*) as cand
-	from  candidat
-	where login = log
-	and mdp = mdp;
-	
-	select count(*) as moni
-	from  moniteur
-	where login = log
-	and mdp = mdp;
-	
-	select count(*) as perso
-	from  personnel
-	where login = log
-	and mdp = mdp;
-	
-	IF cand > 0 and perso = 0 and moni = 0 
-	THEN
-		SET statut ='candidat';
-	ELSE IF cand = 0 and perso > 0 and moni = 0
-	THEN
-		SET statut ="personnel";
-	ELSE IF cand = 0 and perso = 0 and moni > 0
-	THEN
-		SET statut ="moniteur";
-	ELSE
-		SET statut = "ERREUR";
-		
+
+
+DROP PROCEDURE IF EXISTS satut ;
+DELIMITER //
+CREATE PROCEDURE statut
+(log text, mdp text, OUT val text)
+BEGIN
+DECLARE cand int;
+DECLARE perso int;
+DECLARE moni int;
+
+select count(matricule) into cand
+from candidat
+where login = log
+and mdp = mdp;
+
+select count(matricule) into perso
+from personnel
+where login = log
+and mdp = mdp;
+
+select count(matricule) into moni
+from moniteur
+where login = log
+and mdp = mdp;
+
+
+
+	IF cand > 0 and perso = 0 and moni = 0 THEN
+	set val ='candidat';
 	END IF;
+	IF cand = 0 and perso > 0 and moni = 0 THEN
+	set val ='personnel';
+	END IF;
+	IF cand = 0 and perso = 0 and moni > 0 THEN
+	set val ='moniteur';
+	END IF; 
 	
-	return(statut);
-	
-	
-	END //
+END //
 DELIMITER ;
+
+
+call verifstatut('admin', '123', @out_value);
