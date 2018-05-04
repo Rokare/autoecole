af6504f20acc88061fddee6188c53e3a74865dff
Rokare <32900647+Rokare@users.noreply.github.com>
2018-05-05 01:04:00 +0200

    update 1.093

--- Vue/vueNavBarPersonnel.php
+++ Vue/vueNavBarPersonnel.php
@@ -1,3 +1,4 @@
+
 <nav class="navbar navbar-expand-lg bg-dark">
 <a type ="button" class="btn btn-primary" href="index.php">Accueil</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -17,9 +18,10 @@
 </nav>
 
 <h1 class = "text-center">Rechercher les Candidats</h1>
-
+<div class="row">
+<div class="col-lg-5">
 <div class= "row">
-<div class="col-md-6">
+<div class="col-md-12">
  <form method="post" action="">
    <div class="col-md-6">
     <div class="text-center">
@@ -27,32 +29,35 @@
       <input type="text" class="form-control" name="login" placeholder="Login">
     </div>
   </div>
-  
+
      <div class="col-md-6">
     <div class="text-center">
       <label>Nom</label>
       <input type="text" class="form-control" name="nom" placeholder="Nom">
     </div>
   </div>
-  
+
      <div class="col-md-6">
     <div class="text-center">
       <label>Prénom</label>
       <input type="text" class="form-control" name="prenom" placeholder="Prénom">
     </div>
   </div>
-  
+
      <div class="col-md-6">
     <div class="text-center">
       <label>Email</label>
       <input type="text" class="form-control" name="email" placeholder="Email">
     </div>
   </div>
-  
+<br>
+<div class="col-md-6">
   <button type="submit" name="submit" class="btn btn-primary" class="text-center" value = "rechercher">Recherche</button>
+</div>
   <br>
   <br>
 
   </div>
 </form>
+</div>
 </div>
--- Vue/vueResultat.php
+++ Vue/vueResultat.php
@@ -1,7 +1,5 @@
-<?php
-
-?>
-
+<div class="col-md-7">
+  <br><br>
     <div class="col-md-3 col-md-pull-9">
  <table class="table table-sm table-dark">
   <thead>
@@ -11,10 +9,8 @@
       <th scope="col">E-mail</th>
       <th scope="col">Login</th>
       <th scope="col">Adresse</th>
-      <th scope="col">Téléphone</th>
       <th scope="col">Ville</th>
       <th scope="col">Date d'inscription</th>
-      <th scope="col">Mode de facturation</th>
       <th scope="col"></th>
 
 
@@ -27,16 +23,13 @@
 
 
     echo "
-    <tr><td>".$resultats['nom']."</td>
-
+            <tr><td>".$resultats['nom']."</td>
             <td>".$resultats['prenom']."</td>
             <td>".$resultats['email']."</td>
             <td>".$resultats['login']."</td>
             <td>".$resultats['adresse']."</td>
-            <td>".$resultats['telephone']."</td>
             <td>".$resultats['id_ville']."</td>
             <td>".$resultats['date_i']."</td>
-            <td>".$resultats['mode_fact']."</td>
             <td><a href ='indexTiers.php?suppr&sp=".$nbPage."'  type='button' class='btn btn-danger'>Supprimer</a></td>
             <td><a href = ''  type='button' class='btn btn-info'>Modifier</a></td>
 
@@ -54,7 +47,7 @@
 }
    ?>
 </table>
-    </div>
+
 </div>
 
 
@@ -87,3 +80,5 @@
 
   </ul>
 </nav>
+</div>
+</div>
