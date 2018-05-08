0b90ee4e141405eb487a71b2828aa162aad39161
Meudz <32900510+Meudz@users.noreply.github.com>
2018-05-08 15:32:55 +0200

    up

--- Vue/vueNavBarPersonnel.php
+++ Vue/vueNavBarPersonnel.php
@@ -1,4 +1,3 @@
-
 <nav class="navbar navbar-expand-lg bg-dark">
 <a type ="button" class="btn btn-primary" href="index.php">Accueil</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -16,3 +15,44 @@
 			</li>
   </div>
 </nav>
+
+<h1 class = "text-center">Rechercher les Candidats</h1>
+
+<div class= "row">
+<div class="col-md-6">
+ <form method="post" action="">
+   <div class="col-md-6">
+    <div class="text-center">
+      <label>Login</label>
+      <input type="text" class="form-control" name="login" placeholder="Login">
+    </div>
+  </div>
+  
+     <div class="col-md-6">
+    <div class="text-center">
+      <label>Nom</label>
+      <input type="text" class="form-control" name="nom" placeholder="Nom">
+    </div>
+  </div>
+  
+     <div class="col-md-6">
+    <div class="text-center">
+      <label>Prénom</label>
+      <input type="text" class="form-control" name="prenom" placeholder="Prénom">
+    </div>
+  </div>
+  
+     <div class="col-md-6">
+    <div class="text-center">
+      <label>Email</label>
+      <input type="text" class="form-control" name="email" placeholder="Email">
+    </div> 
+  </div>
+  
+  <button type="submit" name="submit" class="btn btn-primary" class="text-center" value = "rechercher">Recherche</button>
+  <br>
+  <br>
+
+  </div>
+</form>
+</div>
