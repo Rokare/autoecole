c6b6b54608418861b7026498b1f11c0c75f3b344
Malek <dahmouche.abdelmalek@gmail.com>
2018-05-23 16:11:25 +0200

    Triggers modifiés

--- Modele/SQL/trigger.sql
+++ Modele/SQL/trigger.sql
@@ -2,44 +2,15 @@
 DROP TRIGGER IF EXISTS insertCandidat;
 
 DELIMITER //
-
+/* TRIGGER QUI INSERE AUTOMATIQUEMENT DANS LA TABLE CANDIDAT AVANT L'INSERTION DANS LA TABLE ETUDIANT */
 CREATE TRIGGER insertCandidat
-AFTER INSERT ON etudiant
-FOR EACH ROW
-    BEGIN
-   	declare nb int;
-
-    SELECT count(*) into nb
-	from candidat
-	where email = new.email
-	or login = new.login
-	or matricule = new.matricule;
-
-    IF nb > 0
-    THEN
-        SIGNAL SQLSTATE '42000'
-        SET MESSAGE_TEXT = " erreur d'insertion dans la table candidat ";
-	ELSE
-		insert into candidat values(new.matricule, new.nom, new.prenom, new.date_n, new.adresse, new.login, new.mdp, new.email, 4, new.telephone, new.id_ville, new.date_i, new.mode_fact);
-    END IF;
-
-
-    END //
-DELIMITER ;
-
-
-DROP TRIGGER IF EXISTS insertEtudiant;
-
-DELIMITER //
-
-CREATE TRIGGER insertEtudiant
 BEFORE INSERT ON etudiant
 FOR EACH ROW
-    BEGIN
-   	declare nb int;
-	declare nb2 int;
+BEGIN
+  declare nb int;
+  declare nb2 int;
 
-    SELECT count(*) into nb
+  SELECT count(*) into nb
 	from etudiant
 	where email = new.email
 	or login = new.login
@@ -51,11 +22,13 @@ FOR EACH ROW
 	or login = new.login
 	or matricule = new.matricule;
 
-    IF nb > 0 or nb2 > 0
+    IF nb > 0 OR nb2 >0
     THEN
         SIGNAL SQLSTATE '42000'
-        SET MESSAGE_TEXT = " erreur d'insertion dans la table etudiant ";
+        SET MESSAGE_TEXT = " erreur d'insertion dans la table candidat ";
 
+    ELSE
+		    insert into candidat values(new.matricule, new.nom, new.prenom, new.date_n, new.adresse, new.login, new.mdp, new.email, 4, new.telephone, new.id_ville, curdate(), new.mode_fact);
     END IF;
 
 
@@ -64,17 +37,52 @@ DELIMITER ;
 
 
 
+DROP TRIGGER IF EXISTS insertSalarie;
+DELIMITER //
+
+/* TRIGGER QUI INSERE AUTOMATIQUEMENT DANS LA TABLE CANDIDAT AVANT L'INSERTION DANS LA TABLE SALARIE */
+BEFORE INSERT ON insertSalarie
+FOR EACH ROW
+
+BEGIN
+    DECLARE nb int;
+    DECLARE nb2 int;
+
+    SELECT count(*) int nb
+    FROM tiers
+    WHERE email = new.email
+    OR login = new.login
+    OR matricule = new.matricule;
+
+    SELECT count(*) into nb2
+  	from tiers
+  	where email = new.email
+  	or login = new.login
+  	or matricule = new.matricule;
+
+    IF nb > 0 OR nb2 > 0
+    THEN
+        SIGNAL SQLSTATE '42000'
+        SET MESSAGE_TEXT = "Erreur d'insertion dans la table tiers";
+    ELSE
+        INSERT INTO candidat VALUES(new.matricule, new.nom, new.prenom, new.date_n, new.adresse, new.login, new.email, 4, new.telephone, new.id_ville, curdate(), new.mode_fact);
+    END IF;
+END //
+DELIMITER ;
+
+
+
 DROP TRIGGER IF EXISTS insertTiers;
 
 DELIMITER //
-
+/* TRIGGER QUI INSERE AUTOMATIQUEMENT DANS LA TABLE CANDIDAT AVANT L'INSERTION DANS LA TABLE TIERS */
 CREATE TRIGGER insertTiers
-AFTER INSERT ON candidat
+BEFORE INSERT ON candidat
 FOR EACH ROW
-    BEGIN
-   	declare nb int;
+BEGIN
+  DECLARE nb int;
 
-    SELECT count(*) into nb
+  SELECT count(*) into nb
 	from tiers
 	where email = new.email
 	or login = new.login
@@ -84,13 +92,15 @@ FOR EACH ROW
     THEN
         SIGNAL SQLSTATE '42000'
         SET MESSAGE_TEXT = " erreur d'insertion dans la table tiers ";
-	ELSE
-		insert into tiers values(new.matricule, new.nom, new.prenom, new.date_n, new.adresse, new.login, new.mdp, new.email, 4, new.telephone, new.id_ville);
+	  ELSE
+		    insert into tiers values(new.matricule, new.nom, new.prenom, new.date_n, new.adresse, new.login, new.mdp, new.email, 4, new.telephone, new.id_ville);
     END IF;
 
 
-    END //
+END //
 DELIMITER ;
+
+
 
 
 
@@ -101,11 +111,11 @@ DELIMITER //
 CREATE TRIGGER insertMoniteur
 BEFORE INSERT ON moniteur
 FOR EACH ROW
-    BEGIN
-   	declare nb int;
+BEGIN
+  declare nb int;
 	declare nb2 int;
 
-    SELECT count(*) into nb
+  SELECT count(*) into nb
 	from moniteur
 	where email = new.email
 	or login = new.login
