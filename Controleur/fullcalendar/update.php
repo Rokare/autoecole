054b232150ddab7f876d6bbe73db6cffd4335e1f
Rokare <32900647+Rokare@users.noreply.github.com>
2018-05-27 17:44:46 +0200

    udpate 1.117

--- Controleur/fullcalendar/update.php
+++ Controleur/fullcalendar/update.php
@@ -6,20 +6,48 @@ $connect = new PDO('mysql:host=localhost;dbname=adlauto', 'root', '');
 
 if(isset($_POST["id"]))
 {
- $query = "
- UPDATE events2 
- SET title=:title, start_event=:start_event, end_event=:end_event 
- WHERE id=:id
- ";
- $statement = $connect->prepare($query);
- $statement->execute(
-  array(
-   ':title'  => $_POST['title'],
-   ':start_event' => $_POST['start'],
-   ':end_event' => $_POST['end'],
-   ':id'   => $_POST['id']
-  )
- );
+
+
+  $req ="select * from moniteur where nom =:nom";
+  $requete = $connect->prepare($req);
+  $requete->execute(
+    array(
+     ':nom' => $_POST['moniteur']
+  ));
+  $rep=$requete->fetch();
+  $mat_m = $rep['matricule'];
+
+
+
+  $req2 ="select count(*) as nb from events2 where mat_m=:moniteur and start_event >= :start_event and end_event <= :end_event";
+  $requete2 = $connect->prepare($req2);
+  $requete->execute(array(
+     ':start_event' => $_POST['start'],
+     ':end_event' => $_POST['end'],
+     ':moniteur' => $mat_m
+    ));
+
+  if($rep2=$requete2->fetch())
+  {
+    $nb = $rep2['nb'];
+  }
+
+
+
+     $query = "
+     UPDATE events2
+     SET title=:title, start_event=:start_event, end_event=:end_event
+     WHERE id=:id
+     ";
+     $statement = $connect->prepare($query);
+     $statement->execute(
+      array(
+       ':title'  => $_POST['title'],
+       ':start_event' => $_POST['start'],
+       ':end_event' => $_POST['end'],
+       ':id'   => $_POST['id']
+      )
+     );
 }
 
 ?>
--- fullcalendar.php
+++ fullcalendar.php
@@ -6,8 +6,9 @@
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha.6/css/bootstrap.css" />
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.18.1/moment.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.4.0/fullcalendar.min.js"></script>
+  <script src=".\Controleur\fullcalendar\js\moment.min.js"></script>
+  <script src=".\Controleur\fullcalendar\js\fullcalendar.min.js"></script>
+  <script src=".\Controleur\fullcalendar\js\locale-all.js"></script>
   <script>
 
 
@@ -29,11 +30,13 @@
 
   $(document).ready(function() {
    var calendar = $('#calendar').fullCalendar({
+     locale:'fr',
+    lang: 'fr',
     editable:true,
     header:{
      left:'prev,next today',
      center:'title',
-     right:'month,agendaWeek,agendaDay'
+     right:'month,agenda,agendaDay'
     },
     events: './Controleur/fullcalendar/load.php?matricule='+$_GET('matricule'),
     selectable:true,
