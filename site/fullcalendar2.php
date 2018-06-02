e37fcac805885fb2271b88f6ce66472a6c4b5e9f
Meudz <32900510+Meudz@users.noreply.github.com>
2018-06-02 19:59:43 +0200

    as

--- site/fullcalendar2.php
+++ site/fullcalendar2.php
@@ -54,7 +54,7 @@
  </head>
  <body>
   <br />
-  <h2 align="center"><a href="#">Jquery Fullcalandar Integration with PHP and Mysql</a></h2>
+  <h2 align="center"><a href="#">Planning Moniteur</a></h2>
   <br />
   <div class="container">
    <div id="calendar"></div>
