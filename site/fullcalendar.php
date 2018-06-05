e4c967612bfee6fa420968e6cda7c7519e4e02d5
Rokare <32900647+Rokare@users.noreply.github.com>
2018-06-05 04:09:11 +0200

    update 1.164

--- site/fullcalendar.php
+++ site/fullcalendar.php
@@ -1,14 +1,20 @@
+<?php session_start(); ?>
 <!DOCTYPE html>
 <html>
  <head>
+
   <title>Jquery Fullcalandar Integration with PHP and Mysql</title>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.4.0/fullcalendar.css" />
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha.6/css/bootstrap.css" />
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+  <link href="Vue/bootstrap-4.0.0-beta.2/dist/css/bootstrap.min.css" rel="stylesheet">
+
+
   <script src=".\Controleur\fullcalendar\js\moment.min.js"></script>
   <script src=".\Controleur\fullcalendar\js\fullcalendar.min.js"></script>
   <script src=".\Controleur\fullcalendar\js\locale-all.js"></script>
+
   <script>
 
 
@@ -137,6 +143,7 @@
   });
 
   </script>
+   <?php include "./Vue/vueNavBarPersonnel.php"; ?>
  </head>
  <body>
   <br />
--- site/index.php
+++ site/index.php
@@ -35,7 +35,14 @@
         <?php
                 $page = (isset($_GET['page']))?$_GET['page'] : 0;
                 $unControleur = new Controleur("localhost","adlauto","root","","candidat");
-                include "Vue/vueBarreNavigation.php";
+                if(isset($_GET['c']))
+                {
+                  include "Vue/vueNavBarPersonnel.php";
+                }
+                else {
+                  include "Vue/vueBarreNavigation.php";
+                }
+
                 if(isset($_POST["submit"]))
                 {
                   extract($_POST);
@@ -45,8 +52,13 @@
                     header("Location:indexTiers.php");
 
                   }
-                  else {
-                    echo "mauvais identifiants";
+                  else
+                  {
+                  echo'
+                  <script>
+                  alert("mauvais identifiants");
+                  </script>';
+
                   }
                 }
                 switch($page)
@@ -114,6 +126,13 @@
 
                     case 3:
                           include "Vue/vueCandidat.php";
+                        break;
+                    case 4 :
+                    echo "<br> Pour utiliser le planning vous devez être connecter <br>";
+                    echo "Redirection... <br>";
+                    echo '<head>
+                      <META HTTP-EQUIV="Refresh" CONTENT="1.5; URL=index.php?p=0">
+                          </head> ';
                         break;
 
 
