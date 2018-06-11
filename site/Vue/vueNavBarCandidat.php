a6f7ea2f77f8879481369d93af708f3d111a007c
Rokare <32900647+Rokare@users.noreply.github.com>
2018-06-11 17:45:03 +0200

    update 1.172

--- site/Vue/vueNavBarCandidat.php
+++ site/Vue/vueNavBarCandidat.php
@@ -2,7 +2,7 @@
 
 
 <nav class="navbar navbar-expand-lg bg-dark">
-<a type ="button" class="btn btn-primary" href="index.php">Accueil</a>
+<a type ="button" class="btn btn-primary" href="indexTiers.php">Accueil</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
